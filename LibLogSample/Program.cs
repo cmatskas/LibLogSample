b3a467cee12a84ca252986b31ccc035e5e5a19b5
Christos Matskas <chris@towzietyke.co.uk>
2015-02-02 19:23:00 +0000

    LibLogSample
    LibLog logger sample with Log4Net

---
+++ LibLogSample/Program.cs
@@ -0,0 +1,16 @@
+﻿using LibLogSample.Logging;
+
+namespace LibLogSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = LogProvider.For<Program>();
+            if (logger.IsDebugEnabled())
+            {
+                logger.Debug("hello world");
+            }
+        }
+    }
+}
